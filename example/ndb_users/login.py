70525cc3e7485ea874d5f90518cf00f3625f9c65
Robert Hodgen <roberthodgen@gmail.com>
2014-12-13 15:29:06 -0500

    JsonLogin now responds to logged in user
    If a user is already logged in an HTTP/1.1 400 Bad Request will be sent as a response.
    
    Updated the docs to reflect this change.
    
    Closes #22

--- example/ndb_users/login.py
+++ example/ndb_users/login.py
@@ -247,7 +247,8 @@ class JsonLogin(webapp2.RequestHandler):
     password = request_object.get('password')
     extended = request_object.get('extended')
     response_object = dict()
-    if email and password:
+    user = users.get_current_user()
+    if email and password and not:
       # Get a User for `email` and `password`
       user = ndb.Key(users.User, users._user_id_for_email(email.lower())).get()
       if user:
@@ -271,8 +272,7 @@ class JsonLogin(webapp2.RequestHandler):
       response_object['login_fail'] = True
       self.response.content_type = 'application/json'
       self.response.out.write(json.dumps(response_object))
-    else:
-      self.abort(400)
+    self.abort(400) # Logged in user, no `email`, or no `password`
 
 
 class LoginCreate(webapp2.RequestHandler):
